0e420a09668a9ad89865d8f349a31f507a59e430
julienESN <julien.esnau@gmail.com>
2025-06-27 14:39:11 +0200

    fixed redis error unable to connect

--- src/app.module.ts
+++ src/app.module.ts
@@ -23,8 +23,8 @@ import { DocumentEventsProcessor } from './document/document-events.processor';
     }),
     BullModule.forRoot({
       connection: {
-        host: 'localhost',
-        port: 6379,
+        host: process.env.REDIS_HOST || 'localhost',
+        port: parseInt(process.env.REDIS_PORT || '6379'),
       },
     }),
     BullModule.registerQueue({
