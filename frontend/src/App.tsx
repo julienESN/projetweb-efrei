df28f525265a30789dcccef3c654822f274b8249
Clement Luke <clement.luke@free.fr>
2025-06-30 11:19:57 +0200

    Front Init

---
+++ frontend/src/App.tsx
@@ -0,0 +1,8 @@
+function App() {
+  return (
+    <div className="bg-red-500 min-h-screen flex items-center justify-center">
+  <h1 className="text-4xl font-bold text-white">Test couleur</h1>
+</div>
+  );
+}
+export default App;
