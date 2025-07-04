e043566d09c47e4ef3e2331ecc96dcd08c95f1aa
Sofiane Fares <79220488+FaresSofiane@users.noreply.github.com>
2025-07-04 10:25:14 +0200

    rajout de postman et cors

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,9 +4,12 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  // Autorise le front (localhost:5173) à accéder à l'API
+  // Autorise le front (localhost:5173 et production) à accéder à l'API
   app.enableCors({
-    origin: ['http://localhost:5173'], // ou ['*'] pour tout autoriser (déconseillé en prod)
+    origin: [
+      'http://localhost:5173', // développement local
+      'https://projetweb-efrei-frontend.onrender.com' // production
+    ],
   });
 
   await app.listen(3000);
